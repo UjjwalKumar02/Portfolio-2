a473b98d42c6f7087fbfb17764aa402b9cd0a178
UjjwalKumar02 <kumarujjwaldos@gmail.com>
2024-08-12 16:33:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document.querySelector(".hide").addEventListener("click", () => {
+  document.querySelector(".mobile-nav").classList.add("show");
+  console.log("heeeeeeeeeeeeeeeeeeeee");
+})
+
+document.querySelector(".m-nav-hide").addEventListener("click", () => {
+  document.querySelector(".mobile-nav").classList.remove("show");
+})
+
+document.querySelector("#li-hide1").addEventListener("click", () => {
+  document.querySelector(".mobile-nav").classList.remove("show");
+})
+document.querySelector("#li-hide2").addEventListener("click", () => {
+  document.querySelector(".mobile-nav").classList.remove("show");
+})
+document.querySelector("#li-hide3").addEventListener("click", () => {
+  document.querySelector(".mobile-nav").classList.remove("show");
+})
+document.querySelector("#li-hide4").addEventListener("click", () => {
+  document.querySelector(".mobile-nav").classList.remove("show");
+})
